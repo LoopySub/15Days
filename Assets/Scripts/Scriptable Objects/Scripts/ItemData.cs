a69664c1c40fd117bb3fdcc0cdb13c6e33fce5ba
LoopySub <153615396+LoopySub@users.noreply.github.com>
2024-02-27 17:05:17 +0900

    [feat] 아이템 추가 및 인벤토리 생성

---
+++ Assets/Scripts/Scriptable Objects/Scripts/ItemData.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum ItemType
+{
+    Evidence,  //단서아이템
+    Equipable, // 장비 아이템
+    Consumable // 소비 아이템
+}
+public enum ConsumableType
+{
+    Hunger, // Food로 치료 (아빠)
+    Health  // FIrst aid kit 로 치료 (딸)
+}
+
+[System.Serializable]
+public class ItemDataConsumable
+{
+    public ConsumableType type;
+    public float value;
+}
+
+[CreateAssetMenu(fileName = "Item", menuName = "New Item")]
+public class ItemData : ScriptableObject
+{
+    [Header("Info")]
+    public string displayName;   // 아이템 이름
+    public string description;   // 아이템 설명
+    public ItemType type;        // 아이템 타입(단서,장비,소비)
+    public Sprite icon;             
+    public GameObject dropPrefab;  
+
+    [Header("Stacking")]
+    public bool canStack;        // 아이템이 쌓일수있는지? Food와 aidKit는 가능
+    public int maxStackAmount;  // 축적 한도
+
+    [Header("Consumable")]
+    public ItemDataConsumable[] consumables;
+
+}

578fe7a4b43a8952434d412dc273841abc1eea63
s8st <153998744+s8st@users.noreply.github.com>
2024-02-29 19:55:54 +0900

    Revert "Merge branch 'main' of https://github.com/LoopySub/15Days"
    This reverts commit a543053c0fef4b9a1eaab394202bd29690a10441, reversing
    changes made to 78e1b69a89e02b8122f9188b4959f4ae08b0ce08.

--- Assets/Scripts/Characters/PlayerInputController.cs
+++ Assets/Scripts/Characters/PlayerInputController.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using static UnityEditor.Progress;
 
 public class PlayerInputController : TopDownCharacterController
 {
-    InteractionManager interact;
     private Camera _camera;
-    private void Awake()
+
+    //private void Awake()
+    //{
+    //    _camera = Camera.main;
+    //}
+
+    protected override void Awake()
     {
+        base.Awake();
         _camera = Camera.main;
     }
+
 
     public void OnMove(InputValue value)
     {
@@ -27,7 +32,7 @@ public class PlayerInputController : TopDownCharacterController
         Vector2 newAim = value.Get<Vector2>();
         Vector2 worldPos = _camera.ScreenToWorldPoint(newAim);
         newAim = (worldPos - (Vector2)transform.position).normalized;
-        
+
         if (newAim.magnitude >= .9f)
         // Vector 값을 실수로 변환
         {
@@ -35,21 +40,9 @@ public class PlayerInputController : TopDownCharacterController
         }
     }
 
-    public void OnInventory(InputValue value)
+    public void OnFire(InputValue value)
     {
-        if (value.isPressed)
-        {
-            Inventory.instance.Toggle();
-        }
+       // Debug.Log("OnFire" + value.ToString());
+        IsAttacking = value.isPressed;
     }
-
-   
-
-
-
-
-    //public void OnFire(InputValue value)
-    //{
-    //    Debug.Log("OnFire" + value.ToString());
-    //}
 }

97394fc71e2c8c0e4c794b1e6e7bb22856f3e2cc
s8st <153998744+s8st@users.noreply.github.com>
2024-02-29 16:41:25 +0900

    Fix_followrange 20 ->5 수정

--- Assets/Scripts/Characters/PlayerInputController.cs
+++ Assets/Scripts/Characters/PlayerInputController.cs
@@ -42,7 +42,7 @@ public class PlayerInputController : TopDownCharacterController
 
     public void OnFire(InputValue value)
     {
-        Debug.Log("OnFire" + value.ToString());
+       // Debug.Log("OnFire" + value.ToString());
         IsAttacking = value.isPressed;
     }
 }

c0b0c3ed9383f5bac134ac2e8701ad7d84318408
s8st <153998744+s8st@users.noreply.github.com>
2024-02-29 16:37:21 +0900

    Fix_플레이어와 총 방향 수정

--- Assets/Scripts/Characters/TopDownAimRotation.cs
+++ Assets/Scripts/Characters/TopDownAimRotation.cs
@@ -31,7 +31,7 @@ public class TopDownAimRotation : MonoBehaviour
         float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
         //armRenderer.flipY = Mathf.Abs(rotZ) > 90f;
-        armRenderer.flipY = Mathf.Abs(rotZ) > 90f;
+        armRenderer.flipY = Mathf.Abs(rotZ) < 90f;
         characterRenderer.flipX = armRenderer.flipY;
         armPivot.rotation = Quaternion.Euler(0, 0, rotZ);
     }
